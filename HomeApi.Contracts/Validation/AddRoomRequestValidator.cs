de3cf08a47ef2b777482ba116b553e646f30ca2b
EVDOC88 <vovan8888@mail.ru>
2023-08-23 18:48:05 +0300

    Добавьте файлы проекта.

---
+++ HomeApi.Contracts/Validation/AddRoomRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using HomeApi.Contracts.Models.Rooms;
+using System.Linq;
+
+namespace HomeApi.Contracts.Validation
+{
+    /// <summary>
+    /// Класс-валидатор запросов на добавление новой комнаты
+    /// </summary>
+    public class AddRoomRequestValidator : AbstractValidator<AddRoomRequest>
+    {
+        public AddRoomRequestValidator() 
+        {
+            RuleFor(x => x.Area).NotEmpty(); 
+            RuleFor(x => x.Name).NotEmpty().Must(BeSupported).WithMessage($"Please choose one of the following locations: {string.Join(", ", Values.ValidRooms)}");
+            RuleFor(x => x.Voltage).NotEmpty();
+            RuleFor(x => x.GasConnected).NotNull();
+        }
+        private bool BeSupported(string location)
+        {
+            // Проверим, содержится ли значение в списке допустимых
+            return Values.ValidRooms.Any(e => e == location);
+        }
+    }
+}
---
+++ HomeApi.Contracts/Validation/EditRoomRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using HomeApi.Contracts.Models.Devices;
+using HomeApi.Contracts.Models.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Validation
+{
+    public class EditRoomRequestValidator : AbstractValidator<EditRoomRequest>
+    {
+        /// <summary>
+        /// Метод, конструктор, устанавливающий правила
+        /// </summary>
+        public EditRoomRequestValidator()
+        {
+            RuleFor(x => x.NewName).Must(BeSupported).WithMessage($"Please choose one of the following locations: {string.Join(", ", Values.ValidRooms)}");
+        }
+
+        /// <summary>
+        ///  Метод кастомной валидации для свойства location
+        /// </summary>
+        private bool BeSupported(string location)
+        {
+            return Values.ValidRooms.Any(e => e == location);
+        }
+    }
+}
